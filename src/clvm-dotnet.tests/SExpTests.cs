348aa2d0fafd7ff3344601b0875f6b158f1dc3aa
kevinonfrontend <kevinonfrontend@outlook.com>
2023-12-29 10:34:25 +0000

    Added SExpTests stubs

--- src/clvm-dotnet.tests/SExpTests.cs
+++ src/clvm-dotnet.tests/SExpTests.cs
@@ -3,6 +3,64 @@ using Xunit;
 
 public class SExpTests
 {
+    // [Fact]
+    // public void test_case_1()
+    // {
+    //     var sexp = SExp.To("foo");
+    //     var t1 = SExp.To([1, sexp]);
+    //     ValidateSExp(t1);
+    // } 
+
+    [Fact]
+    public void wrap_sexp()
+    {
+        // it's a bit of a layer violation that CLVMObject unwraps SExp, but we
+        // rely on that in a fair number of places for now. We should probably
+        // work towards phasing that out
+        // var o = new CLVMObject(SExp.To(1));
+        // Assert.Equal(o.Atom, bytes([1]);
+    }
+
+    [Fact]
+    public void list_conversions()
+    {
+        // def test_list_conversions(self):
+        // a = SExp.to([1, 2, 3])
+        // assert print_tree(a) == "(1 (2 (3 () )))"
+    }
+    
+    [Fact]
+    public void string_conversions()
+    {
+        // SExp a = SExp.To("foobar");
+        // Assert.Equal("foobar".EncodeToUtf8(), a.AsAtom());
+    }
+
+    [Fact]
+    public void int_conversions()
+    {
+        // SExp a = SExp.To(1337);
+        // byte[] expected = new byte[] { 0x5, 0x39 };
+        // Assert.Equal(expected, a.AsAtom());
+    }
+    
+    [Fact]
+    public void TestNoneConversions()
+    {
+        // SExp a = SExp.To(null);
+        // byte[] expected = new byte[0];
+        // Assert.Equal(expected, a.AsAtom());
+    }
+    
+    [Fact]
+    public void empty_list_conversions()
+    {
+        // SExp a = SExp.To(new object[] { });
+        // byte[] expected = new byte[0];
+        // Assert.Equal(expected, a.AsAtom());
+    }
+    
+    #region test helpers that should probably go into SExp object
     private string PrintLeaves(SExp tree)
     {
         var a = tree.AsAtom();
@@ -53,4 +111,55 @@ public class SExpTests
         ret += ")";
         return ret;
     }
+
+    public static void ValidateSExp(SExp sexp)
+    {
+        Stack<SExp> validateStack = new Stack<SExp>();
+        validateStack.Push(sexp);
+
+        while (validateStack.Count > 0)
+        {
+            SExp v = validateStack.Pop();
+            if (!(v is SExp))
+            {
+                throw new Exception("Validation failed: v is not an instance of SExp.");
+            }
+
+            if (v.Pair != null)
+            {
+                if (!(v.Pair is Tuple<object, object>))
+                {
+                    throw new Exception("Validation failed: v.Pair is not a Tuple.");
+                }
+
+                var (v1, v2) = v.Pair;
+
+                if (!HelperFunctions.LooksLikeCLVMObject((v1)) || !HelperFunctions.LooksLikeCLVMObject(v2))
+                {
+                    throw new Exception("Validation failed: v1 and v2 do not look like CLVM objects.");
+                }
+
+                SExp s1 = v1 as SExp;
+                SExp s2 = v2 as SExp;
+
+                if (s1 != null && s2 != null)
+                {
+                    validateStack.Push(s1);
+                    validateStack.Push(s2);
+                }
+                else
+                {
+                    throw new Exception("Validation failed: s1 and s2 are not instances of SExp.");
+                }
+            }
+            else
+            {
+                if (!(v.Atom is byte[]))
+                {
+                    throw new Exception("Validation failed: v.Atom is not a byte array.");
+                }
+            }
+        }
+    }
+    #endregion
 }
