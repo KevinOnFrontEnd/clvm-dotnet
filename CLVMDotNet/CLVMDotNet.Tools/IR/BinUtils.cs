9f8854b71710e207bf974364ef86d51f96e36065
kev <kev@darkhorse>
2024-01-17 19:10:38 +0000

    Added compilete functionality

---
+++ CLVMDotNet/CLVMDotNet.Tools/IR/BinUtils.cs
@@ -0,0 +1,11 @@
+namespace CLVMDotNet.Tools.IR;
+
+public class BinUtils
+{
+    //Assembly
+    //Disassemble
+    //disassemble_to_ir
+    //type_for_atom
+    //printable_chars
+    //assemble_from_ir
+}
---
+++ CLVMDotNet/CLVMDotNet.Tools/IR/Clvmc.cs
@@ -0,0 +1,8 @@
+namespace CLVMDotNet.Tools.IR;
+
+public class Clvmc
+{
+    //compile_clvm_text
+    //compile_clvm
+    //find_files
+}
