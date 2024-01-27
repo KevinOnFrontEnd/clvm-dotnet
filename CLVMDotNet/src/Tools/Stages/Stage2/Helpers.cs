dea43043cc7c1ed05996eb420b6b1f06cb57ed01
kev <kev@darkhorse>
2024-01-27 16:52:23 +0000

    Added Quote

--- CLVMDotNet/src/Tools/Stages/Stage2/Helpers.cs
+++ CLVMDotNet/src/Tools/Stages/Stage2/Helpers.cs
@@ -1,6 +1,14 @@
+using CLVMDotNet.CLVM;
+
 namespace CLVMDotNet.Tools.Stages.Stage2;
 
-public class Helpers
+public static class Helpers
 {
-    
+    public static byte QUOTE_ATOM => Keywords.KEYWORD_TO_ATOM["q"];
+    public static byte APPLY_ATOM => Keywords.KEYWORD_TO_ATOM["a"];
+
+    public static Tuple<byte, SExp> Quote(SExp sexp)
+    {
+        return Tuple.Create(QUOTE_ATOM, sexp);
+    }
 }
